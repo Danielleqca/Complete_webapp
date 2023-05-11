d15be6be1e27a8d7aa32b9e2dc278fd37ec83f66
Victor Amarante <vafstats@gmail.com>
2023-05-11 11:28:17 -0300

    Update 5_Tratamento_Base_de_Dados_(Serviços Internos).py

--- pages/5_Tratamento_Base_de_Dados_(Serviços Internos).py
+++ pages/5_Tratamento_Base_de_Dados_(Serviços Internos).py
@@ -11,7 +11,7 @@ from utils import bg_page
 def generate_excel_download_link(df):
     hoje = date.today()
     towrite = BytesIO()
-    df.to_excel(towrite, encoding="utf-8", index=False, header=True)  # write to BytesIO buffer
+    df.to_excel(towrite, index=False, header=True)  # write to BytesIO buffer
     towrite.seek(0)  # reset pointer
     b64 = base64.b64encode(towrite.read()).decode()
     href = f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{b64}" download="BASE_TRATADA_GERAL_{hoje}.xlsx">Download Excel File</a>'
@@ -20,7 +20,7 @@ def generate_excel_download_link(df):
 def generate_excel_download_link_bahia(df):
     hoje = date.today()
     towrite = BytesIO()
-    df.to_excel(towrite, encoding="utf-8", index=False, header=True)  # write to BytesIO buffer
+    df.to_excel(towrite, index=False, header=True)  # write to BytesIO buffer
     towrite.seek(0)  # reset pointer
     b64 = base64.b64encode(towrite.read()).decode()
     href = f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{b64}" download="BASE_TRATADA_BAHIA_{hoje}.xlsx">Download Bahia</a>'
