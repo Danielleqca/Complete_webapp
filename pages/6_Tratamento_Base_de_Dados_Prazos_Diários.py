b9e1ffb9a897132f0a41223cb632a08f29937720
Victor Amarante <vafstats@gmail.com>
2023-05-16 11:02:51 -0300

    Update 6_Tratamento_Base_de_Dados_Prazos_Diários.py

--- pages/6_Tratamento_Base_de_Dados_Prazos_Diários.py
+++ pages/6_Tratamento_Base_de_Dados_Prazos_Diários.py
@@ -10,7 +10,7 @@ from io import StringIO, BytesIO
 def generate_excel_download_link_agendados(df):
     hoje = date.today()
     towrite = BytesIO()
-    df.to_excel(towrite, encoding="utf-8", index=False, header=True)  # write to BytesIO buffer
+    df.to_excel(towrite, index=False, header=True)  # write to BytesIO buffer
     towrite.seek(0)  # reset pointer
     b64 = base64.b64encode(towrite.read()).decode()
     href = f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{b64}" download="BASE_TRATADA_AGENDADOS_{hoje}.xlsx">Download Excel File</a>'
@@ -19,7 +19,7 @@ def generate_excel_download_link_agendados(df):
 def generate_excel_download_link_pendentes(df):
     hoje = date.today()
     towrite = BytesIO()
-    df.to_excel(towrite, encoding="utf-8", index=False, header=True)  # write to BytesIO buffer
+    df.to_excel(towrite, index=False, header=True)  # write to BytesIO buffer
     towrite.seek(0)  # reset pointer
     b64 = base64.b64encode(towrite.read()).decode()
     href = f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{b64}" download="BASE_TRATADA_PENDENTES_{hoje}.xlsx">Download Excel File</a>'
