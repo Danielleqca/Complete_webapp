228715df51f877775bb017efb6e315539a773acd
Jean QCA <jeanaraujo@queirozcavalcanti.adv.br>
2024-09-06 14:05:22 -0300

    add eq de base

---
+++ pages/9_Auditoria_de_equalizacao_de_base.py
@@ -0,0 +1,170 @@
+import pandas as pd
+import streamlit as st
+import io
+from utils import bg_page
+
+st.set_page_config(
+    page_title="Auditoria de Equalização de Base",
+    page_icon='qca_logo_2.png',
+    layout="wide",
+)
+
+bg_page('bg_dark.png')
+
+hide_menu = """
+<style>
+#MainMenu {
+    visibility:visible;
+}
+footer {
+    visibility:visible;
+}
+footer:before {
+    content:'Desenvolvido pela Eficiência Jurídica - Controladoria Jurídica';
+    display:block;
+    position:relative;
+    color:#6c6a76;
+}
+</style>
+"""
+
+with st.sidebar:
+    st.image('qca_logo_2.png')
+    st.title('Auditoria de Equalização de Base')
+    st.info('Automação Desenvolvida para Automatizar parte da Auditoria de Eq. de Base.')
+    
+st.title("Equalização de Base")
+
+st.subheader("1. Faça o upload da base do cliente:")
+st.markdown(hide_menu, unsafe_allow_html=True)
+base_cliente = st.file_uploader("Selecione o arquivo da base do cliente", type=["xlsx"])
+
+st.subheader("2. Faça o upload da base do Performa:")
+st.markdown(hide_menu, unsafe_allow_html=True)
+base_performa = st.file_uploader("Selecione o arquivo da base do Performa", type=["xlsx"])
+
+# Initialize session state variables
+if 'processado' not in st.session_state:
+    st.session_state.processado = False
+if 'resultados_nulos_performa' not in st.session_state:
+    st.session_state.resultados_nulos_performa = None
+if 'resultados_nulos_cliente' not in st.session_state:
+    st.session_state.resultados_nulos_cliente = None
+
+class Automacao:
+    def __init__(self):
+        self.df_cliente = None
+        self.df_performa = None
+
+    def normalizar_num_processo(self, df, coluna_num_processo):
+        if df is self.df_performa:
+            nova_coluna = f'{coluna_num_processo} na Base do Performa'
+        elif df is self.df_cliente:
+            nova_coluna = f'{coluna_num_processo} na Base do Cliente'
+        df[nova_coluna] = df[coluna_num_processo]
+        
+        df[coluna_num_processo] = df[coluna_num_processo].str.replace(r'[^\w\s]', '', regex=True)
+        df[coluna_num_processo] = df[coluna_num_processo].str.replace(r'\s+', '', regex=True)
+        
+    def processar_automacao(self):
+        if self.df_cliente is None or self.df_performa is None:
+            st.error("Por favor, faça o upload de ambos os arquivos antes de processar.")
+            return
+        
+        st.spinner("Processando dados...")
+        self.normalizar_num_processo(self.df_cliente, 'Numeração Única')
+        self.normalizar_num_processo(self.df_performa, 'Número Processo')
+
+        # TRATAMENTOS BASE PERFORMA
+        self.df_performa = self.df_performa[['ID', 'NPC', 'Número Processo', 'Data Cadastro', 'Data Revisão', 'Fase', 'Célula', 'Advogado Responsável', 'Cliente', 'Centro de Custo',
+                                            'Tipo Processo', 'Status', 'Número Processo na Base do Performa']]
+
+        try:
+            df_performa_procv = pd.merge(self.df_performa, self.df_cliente[['Cód. Causa']], left_on='NPC', right_on='Cód. Causa', how="left")
+        except Exception as e:
+            st.error(f'Não foi possível realizar o PROCV na coluna de NPC da base do Performa: {e}')
+            return
+
+        try:
+            df_performa_procv = pd.merge(df_performa_procv, self.df_cliente[['Numeração Única']], left_on='Número Processo', right_on='Numeração Única', how="left")
+        except Exception as e:
+            st.error(f'Não foi possível realizar o PROCV na coluna Número Processo da base do Performa: {e}')
+            return
+
+        condition = df_performa_procv['Cód. Causa'].isna() & df_performa_procv['Numeração Única'].isna()
+        st.session_state.resultados_nulos_performa = df_performa_procv[condition].copy()
+        st.session_state.n_resultados_nulos_performa = st.session_state.resultados_nulos_performa.shape[0]
+        st.session_state.total_base_performa = self.df_performa.shape[0]
+
+        # TRATAMENTOS BASE CLIENTE
+        try:
+            df_cliente_procv = pd.merge(self.df_cliente, self.df_performa[['NPC']], left_on='Cód. Causa', right_on='NPC', how="left")
+        except Exception as e:
+            st.error(f'Não foi possível realizar o PROCV na coluna de NPC da base do cliente: {e}')
+            return
+
+        try:
+            df_cliente_procv = pd.merge(df_cliente_procv, self.df_performa[['Número Processo']], left_on='Numeração Única', right_on='Número Processo', how="left")
+        except Exception as e:
+            st.error(f'Não foi possível realizar o PROCV na coluna Número Processo da base do cliente: {e}')
+            return
+
+        condition = df_cliente_procv['NPC'].isna() & df_cliente_procv['Número Processo'].isna()
+        st.session_state.resultados_nulos_cliente = df_cliente_procv[condition].copy()
+        st.session_state.n_resultados_nulos_cliente = st.session_state.resultados_nulos_cliente.shape[0]
+        st.session_state.total_base_cliente = self.df_cliente.shape[0]
+
+    def gerar_arquivo_excel(self, df, nome_arquivo):
+        buffer = io.BytesIO()
+        with pd.ExcelWriter(buffer, engine='openpyxl') as writer:
+            df.to_excel(writer, index=False, sheet_name='Sheet1')
+        buffer.seek(0)
+        return buffer
+
+    def exibir_resultados(self):
+        if st.session_state.total_base_performa == 0 or st.session_state.total_base_cliente == 0:
+            st.error("Por favor, processe os dados antes de exibir os resultados.")
+            return
+        
+        st.write(f'Base do Performa: {st.session_state.total_base_performa} processos')
+        st.write(f'Base do Cliente: {st.session_state.total_base_cliente} processos')
+        diferenca = st.session_state.total_base_performa - st.session_state.total_base_cliente
+        st.write(f'Diferença: {diferenca}')
+        st.write(f'Temos {st.session_state.n_resultados_nulos_performa} processos na base do Performa que não encontramos no cliente.')
+        st.write(f'Temos {st.session_state.n_resultados_nulos_cliente} processos na base do cliente que não encontramos no Performa.')
+
+# Criar uma instância da classe
+automacao = Automacao()
+
+# Botão de processar automação
+if st.button('Iniciar Automação'):
+    if base_cliente and base_performa:
+        automacao.df_cliente = pd.read_excel(base_cliente)
+        automacao.df_performa = pd.read_excel(base_performa)
+        automacao.processar_automacao()
+        st.session_state.processado = True
+
+        # Exibir resultados automaticamente após o processamento
+        automacao.exibir_resultados()
+    else:
+        st.error("Por favor, faça o upload de ambos os arquivos antes de processar.")
+
+# Exibir botões apenas após o processamento
+if st.session_state.processado:
+    if st.session_state.resultados_nulos_performa is not None:
+        resultado_performa = automacao.gerar_arquivo_excel(st.session_state.resultados_nulos_performa, 'base_performa_auditada.xlsx')
+        st.download_button(
+            label="Download Base do Performa Auditada",
+            data=resultado_performa,
+            file_name="base_performa_auditada.xlsx",
+            mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+        )
+    
+    if st.session_state.resultados_nulos_cliente is not None:
+        resultado_cliente = automacao.gerar_arquivo_excel(st.session_state.resultados_nulos_cliente, 'base_cliente_auditada.xlsx')
+        st.download_button(
+            label="Download Base do Cliente Auditada",
+            data=resultado_cliente,
+            file_name="base_cliente_auditada.xlsx",
+            mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+        )
